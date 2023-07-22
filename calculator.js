5621d43fe02efc496f772d052db31a203c6e1f15
N335T3R <n335t3r@gmail.com>
2023-07-22 15:02:05 -0400

    Basic operation functions designed

---
+++ calculator.js
@@ -0,0 +1,18 @@
+let num1 = 3;
+let num2 = 5;
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function dif(num1, num2) {
+    return num1 - num2;
+}
+
+function prod() {
+    return num1 * num2;
+}
+
+function divi() {
+    return num1 / num2;
+}
