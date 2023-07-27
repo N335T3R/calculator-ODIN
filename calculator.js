d2c823643450acddc37c952b06e722b2cc5872cc
N335T3R <n335t3r@gmail.com>
2023-07-27 00:00:26 -0400

    Equations now accept multi-digit integers.

--- calculator.js
+++ calculator.js
@@ -1,30 +1,39 @@
 let num1;
-let num2;
 let operator;
+let result;
+
+let value = [];
+let displayValue;
 
 const screen = document.querySelector(".screen");
-const keys = [...document.querySelectorAll(".btn")];
-//const ops = [...document.querySelectorAll(".op")];
+screen.textContent = displayValue;
 
-const equals = document.querySelector("#enter");
-const clear = document.querySelector("#clear");
+const keys = [...document.querySelectorAll(".btn")];
+const ops = [...document.querySelectorAll(".op")];
+const equals = document.querySelector(".enter");
+const clear = document.querySelector(".clear");
 
 
 //gather equation values
 for (let i = 0; i < keys.length; i++) {
     keys[i].addEventListener("click", () => {
-        if (!num1) {
-            num1 = Number(keys[i].textContent);
-            screen.textContent = num1;
-        } else if (num1 && !operator) {
-            operator = keys[i].textContent;
-            screen.textContent = operator;
-        } else if (operator && !num2) {
-            num2 = Number(keys[i].textContent);
-            screen.textContent = num2;
-        }
+        value[i] = keys[i].textContent;
+        displayValue = value.toString();
+        displayValue = displayValue.replaceAll(",", "");
     });
 }
+
+for (let i = 0; i < ops.length; i++) {
+        ops[i].addEventListener("click", () => {
+            num1 = Number(displayValue);
+            operator = ops[i].textContent;
+            screen.textContent = num1 + operator;
+            displayValue = "";
+            value.length = 0;
+        });
+}
+
+
 
 
 //4 basic math operations function
@@ -47,8 +56,6 @@ function divide(num1, num2) {
 
 //Operate function
 function operate(num1, operator, num2) {
-    let result; 
-
     if (operator == "+") {
         result = add(num1, num2);
         screen.textContent = result;
@@ -66,11 +73,8 @@ function operate(num1, operator, num2) {
 
 //enter button calls operate()
 equals.addEventListener("click", () => {
-    if (!num1 || !operator || !num2) {
-        screen.textContent = "Sorry. Enter a valid equation including 2 integers and an operator.";
-    } else {
-        operate(num1, operator, num2);
-    }
+    num2 = Number(displayValue);
+    operate(num1, operator, num2);
 });
 
 //clear calculator
@@ -79,4 +83,5 @@ clear.addEventListener("click", () => {
     operator = "";
     num2 = "";
     screen.textContent = "";
-})
+    value = value.length = 0;
+});
